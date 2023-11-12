fc32c212a66fe23af34c9dc7e753a7b6a4782ad1
dlrjs2360 <lhg3670@naver.com>
2023-11-12 15:03:09 +0900

    feat: 음료만 주문 불가능 조건 추가
    음료만 주문할 수 없다는 조건을 추가했습니다. 주문내역을 순회하며 주문 내역 중 음료 카테고리만 있다면 익셉션을 발생합니다.

--- src/main/java/christmas/validator/TotalOrderValidator.java
+++ src/main/java/christmas/validator/TotalOrderValidator.java
@@ -5,21 +5,33 @@ import christmas.constant.EventInfo;
 import christmas.constant.Menu;
 import christmas.domain.Order;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class TotalOrderValidator implements Validator<List<Order>> {
 
-    int totalAmount = 0;
-    HashMap<String, Boolean> menuCheck = new HashMap<>();
+    int totalAmount;
+    HashMap<String, Boolean> menuCheck;
+    Set<Menu.Category> categoryCheck;
 
     @Override
     public void validate(List<Order> orders) {
+        init();
         for (Order order : orders) {
             validateMenuCheck(order.getMenu().getName());
             validateTotalAmount(totalAmount + order.getAmount());
             totalAmount += order.getAmount();
             menuCheck.put(order.getMenu().getName(), true);
+            categoryCheck.add(order.getMenu().getCategory());
         }
+        validateOnlyDrink();
+    }
+
+    private void init() {
+        totalAmount = 0;
+        menuCheck = new HashMap<>();
+        categoryCheck = new HashSet<>();
     }
 
     private void validateTotalAmount(int totalAmount) {
@@ -30,6 +42,12 @@ public class TotalOrderValidator implements Validator<List<Order>> {
 
     private void validateMenuCheck(String menuName) {
         if (menuCheck.getOrDefault(menuName, false)) {
+            throwException(ErrorMessage.INVALID_ORDER);
+        }
+    }
+
+    private void validateOnlyDrink() {
+        if (categoryCheck.size() == 1 && categoryCheck.contains(Menu.Category.DRINK)) {
             throwException(ErrorMessage.INVALID_ORDER);
         }
     }
